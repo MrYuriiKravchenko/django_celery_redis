bb69eec82e04f66fa5079e3ffe5cc10b20821ddc
MrYuriiKravchenko <MisterKrava@yandex.ru>
2024-09-30 19:13:49 +0300

    first

---
+++ django_celery/django_celery/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from publish.views import view_post
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('<slug:slug>/', view_post, name='view_post'),
+]
